20e5357ac18a17415f86205f5ddb5b258860d8c0
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2023-05-23 12:53:28 -0600

    init

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService
+{
+    recipesChanged = new Subject<Recipe[]>();
+
+    /*private recipes: Recipe[] = [
+      new Recipe(
+        'Test Recipe',
+        'This is a test',
+        'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2019/09/enchiladas-con-salsa-guajillo.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('Fries', 20)
+        ]),
+  
+      new Recipe(
+        'Test Another Recipe',
+        'This is another test',
+        'https://assets.unileversolutions.com/recipes-v2/218094.jpg?imwidth=1200',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 2)
+        ])
+    ];*/
+    private recipes: Recipe[] = [];
+
+    constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[])
+    {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    getRecipes()
+    {
+        return this.recipes.slice();
+    }
+
+    getRecipe(index: number)
+    {
+        return this.recipes[index];
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[])
+    {
+        this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe)
+    {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe)
+    {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number)
+    {   
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+}
---
+++ src/app/shared/loading-loader/loading-loader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-loading-loader',
+    template: '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>',
+    styleUrls: ['./loading-loader.component.css']
+
+})
+export class LoadingLoaderComponent {
+
+}
